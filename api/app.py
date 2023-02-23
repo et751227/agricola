700f08f4c81ef8a07386f08b4fc0ad937b543bcf
et751227 <36990569+et751227@users.noreply.github.com>
2023-02-23 17:00:46 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -11,38 +11,11 @@ handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 app = Flask(__name__)
  
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
- 
-  
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
- 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
- 
-    return 'OK' 
-
 #首頁測試
 @app.route('/')
 def home():
     return 'Hello, World!'
-#    return line_bot_api
-#    return handler
- 
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+
 
 if __name__ == "__main__":
     app.run()
