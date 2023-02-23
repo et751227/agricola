00252cf15af9bba2fd1b8df3237e07475d541b37
et751227 <36990569+et751227@users.noreply.github.com>
2023-02-23 16:18:02 +0800

    主程式編輯

--- api/app.py
+++ api/app.py
@@ -1,33 +1,32 @@
 #載入LineBot所需要的模組
 from flask import Flask, request, abort
- 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+
+import os
+
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
 app = Flask(__name__)
  
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('mfhl7PgbcNUgrQtrOYY41eM4QFItaevgruxSqBysuQQnd/dNC5+PLcGbOQWpJDukJiNjo3E1ajdS6ZHG4TmIGeD4KcInWRyqcpEjDSlEOzvsjPBZxHGGSr2ck/ULU5ktwwXb9r6r6AU5GHvoHuFmMgdB04t89/1O/w1cDnyilFU=')
- 
-# 必須放上自己的Channel Secret
-handler = WebhookHandler('64cd78a666a16bef0e5cf97957ef1e18')
-
-line_bot_api.push_message('Ubc9d9f76cfd0b91f30d4fff7839ed4b0', TextSendMessage(text='農家樂機器人建置中'))
-
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
+
+@app.route('/')
+
+def home():
+    return 'Hello, World!'
+    
+@app.route("/webhook", methods=['POST'])
+
 @handler.add(MessageEvent, message=TextMessage)
+
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token,message)
     
-    #主程式
-import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
