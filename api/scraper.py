c87d104bba3c149405e2b844c7a66a42c981cc79
et751227 <36990569+et751227@users.noreply.github.com>
2023-02-25 20:30:23 +0800

    Update scraper.py

--- api/scraper.py
+++ api/scraper.py
@@ -14,27 +14,29 @@ class Stock(ABC):
 class YahooStock(Stock):
 
     def scrape(self):
-    
-        url = "https://tw.stock.yahoo.com/quote/" + self.stockTicket
         
-        content += url
+        content += self.stockTicket
         
-        response = requests.get(
-            "https://tw.stock.yahoo.com/quote/" + self.stockTicket )
+        #url = "https://tw.stock.yahoo.com/quote/" + self.stockTicket
+        
+        #content += url
+        
+        #response = requests.get(
+        #    "https://tw.stock.yahoo.com/quote/" + self.stockTicket )
             
-        soup = BeautifulSoup(response.content, "html.parser")
+        #soup = BeautifulSoup(response.content, "html.parser")
     
-        cards = soup.find_all(
-            'div',{'class':'D(f) Ai(fe) Mb(4px)'})
+        #cards = soup.find_all(
+        #    'div',{'class':'D(f) Ai(fe) Mb(4px)'})
     
-        content += "沒有找到網頁"
+        #content += "沒有找到網頁"
     
-        for card in cards:
+        #for card in cards:
     
-            price = card.find( #股票價格
-                "span",{"class":"Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)"}).getText()
+        #    price = card.find( #股票價格
+        #        "span",{"class":"Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)"}).getText()
     
-        content += f"股票價格:{price}"
+        #content += f"股票價格:{price}"
       
         return content
     
