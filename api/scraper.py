fd2529ae12b84e17033598938ec7d12a502e186f
et751227 <36990569+et751227@users.noreply.github.com>
2023-02-25 21:32:31 +0800

    更多股票資訊

--- api/scraper.py
+++ api/scraper.py
@@ -14,29 +14,31 @@ class Stock(ABC):
 class YahooStock(Stock):
 
     def scrape(self):
+    
         content = ""
-        #content += "我是yahoo"
-        
-        #url = "https://tw.stock.yahoo.com/quote/" + self.stockTicket
-        
-        #content += url
-        
+   
         response = requests.get(
             "https://tw.stock.yahoo.com/quote/" + self.stockTicket )
             
         soup = BeautifulSoup(response.content, "html.parser")
     
+        #cards = soup.find_all(
+        #    'div',{'class':'D(f) Ai(fe) Mb(4px)'})
         cards = soup.find_all(
-            'div',{'class':'D(f) Ai(fe) Mb(4px)'})
-    
-        #content += "沒有找到網頁"
-    
+            'div',{'class':'Mb($m-module-24)'})
+            
         for card in cards:
-    
+            
+            stock_name = card.find( #股票名稱
+                "h1",{"class":"C($c-link-text) Fw(b) Fz(24px) Mend(8px)"}).getText()
+            
+            stock_ticket = card.find( #股票代號
+                "span",{"class":"C($c-icon) Fz(24px) Mend(20px)"}).getText()
+                
             price = card.find( #股票價格
                 "span",{"class":"Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)"}).getText()
     
-        content += f"股票價格:{price}"
+        content += f"股票名稱:{stock_name} \n 股票代號:{stock_ticket} \n 股票價格:{price}"
       
         return content
     
