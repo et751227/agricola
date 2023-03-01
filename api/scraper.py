a2df56a8bb282e29799cb616dae312b22b5d1dfb
et751227 <36990569+et751227@users.noreply.github.com>
2023-03-01 15:48:15 +0800

    Update scraper.py

--- api/scraper.py
+++ api/scraper.py
@@ -58,15 +58,15 @@ class YahooStock(Stock):
         stock_dividends_season = stock_dividends.find('div',{"class":'D(f) W(84px) Ta(start)'}).get_text()
         
         content += f"股票名稱:{stock_name} \n"\
-                    "股票代號:{stock_ticket} \n"\
-                    "股票價格:{price} \n"\
+                    f"股票代號:{stock_ticket} \n"\
+                    f"股票價格:{price} \n"\
                     "------------------------ \n"\
-                    "股利所屬期間:{stock_dividends_season} \n"\
-                    "現金股利:{stock_dividends_money} \n"\
-                    "股票股利:{stock_dividends_son} \n"\
-                    "除息日:{stock_dividends_delete} \n"\
-                    "發放日:{stock_dividends_get} \n"\
-                    "填息天數:{stock_dividends_recover} \n"
+                    f"股利所屬期間:{stock_dividends_season} \n"\
+                    f"現金股利:{stock_dividends_money} \n"\
+                    f"股票股利:{stock_dividends_son} \n"\
+                    f"除息日:{stock_dividends_delete} \n"\
+                    f"發放日:{stock_dividends_get} \n"\
+                    f"填息天數:{stock_dividends_recover} \n"
       
         return content
     
