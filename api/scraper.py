1a30a9beb133d3a0943a53e579f7841aca723753
et751227 <36990569+et751227@users.noreply.github.com>
2023-03-01 16:11:22 +0800

    Update scraper.py

--- api/scraper.py
+++ api/scraper.py
@@ -53,7 +53,7 @@ class YahooStock(Stock):
         
         dividends_soup = BeautifulSoup(dividends_response.content, "html.parser")
                 
-        stock_dividends = dividends_soup.find('ul',{"class":'List(n)'})
+        stock_dividends = dividends_soup.find_all('ul',{"class":'List(n)'})
         
         stock_dividends_season = stock_dividends.find('div',{"class":'D(f) W(84px) Ta(start)'}).get_text()
         stock_dividends_money = stock_dividends.find('div',{"class":'Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend($m-table-cell-space) Mend(0):lc Miw(62px)'}).get_text()
